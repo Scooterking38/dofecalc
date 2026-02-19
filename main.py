f15a5e8fbcdf0b6b76d2fd2024e48e03c344d87f
Scooterking38 <161932463+Scooterking38@users.noreply.github.com>
2026-02-19 15:31:07 +0000

    Fix indentation in match cases for calculator

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ while True:
   
     choice = input("\nEnter 1 for Plus, 2 for Minus, 3 for Times: ")
     match choice:
-      case "1":
-        print(int(num1) + int(num2))
+        case "1":
+            print(int(num1) + int(num2))
   
-      case "2":
-        print(int(num1) - int(num2))
+        case "2":
+            print(int(num1) - int(num2))
 
-      case "3":
-        print(int(num1) * int(num2))
+        case "3":
+            print(int(num1) * int(num2))
 
-      case _:
-        print("Please try again")
+        case _:
+            print("Please try again")
       
     print()

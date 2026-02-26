71668d01c38c574e80d8fcea544dfa1d1f77f309
Rylee B <2burtonr@qegs.email>
2026-02-26 16:27:11 +0000

    Refactor calculator to use list of operations
    Refactor calculator operations to use a list of operators and improve input handling.

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 print("Welcome to Calculator!")
 while True:
-    num1 = input("Please Enter A Number: ")
-    num2 = input("Please Enter A Number: ")
-  
-    choice = input("\nEnter 1 for Plus, 2 for Minus, 3 for Times: ")
+    num1 = int(input("Please Enter A Number: "))
+    num2 = int(input("Please Enter A Number: "))
+
+
+    ops = ['+', '-', 'x', '/']
+
+    for number in range(4):
+        print(str(number + 1) + " For " + ops[number])
+    
+    choice = input("\nEnter An Operation: ")
     match choice:
         case "1":
-            print(int(num1) + int(num2))
+            print(num1 + num2)
   
         case "2":
-            print(int(num1) - int(num2))
+            print(num1 - num2)
 
         case "3":
-            print(int(num1) * int(num2))
+            print(num1 * num2)
+
+        case "4":
+            print(num1 / num2)
 
         case _:
             print("Please try again")

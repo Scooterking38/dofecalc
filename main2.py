e60a44e791a1c5071ac94f3c775fe59898a198fa
Rylee B <2burtonr@qegs.email>
2026-03-05 16:36:31 +0000

    Add comments for clarity in main2.py

--- main2.py
+++ main2.py
@@ -1,32 +1,33 @@
-print("welcome to the cooler calc")
+print("welcome to the cooler calc") #tells people what the program is
 
-while True:
-    num1 = int(input("enter a num: "))
-    num2 = int(input("enter second num: "))
+while True: #means the program repeats forever without restarting
 
-    ops = ["+","-","*","/"]
+    num1 = int(input("enter a num: ")) #sets num1 to an integer which the user enters
+    num2 = int(input("enter second num: ")) #sets num2 to an integer which the user enters
 
-    for num in range(4):
-        print(num+1,"for",ops[num])
+    ops = ["+","-","*","/"] #sets the variable ops to a list of the available operations
 
-    choice = int(input("\nwhat operation: "))
+    for num in range(4): #does the loop four times
+        print(num+1,"for",ops[num]) #prints the number in the loop and the corresponding operator
 
-    match choice:
+    choice = int(input("\nwhat operation: ")) #asks the user which operation they want to use and converts it to an integer
+
+    match choice: #tells the program what to do depending on the number entered
 
         case 1:
-            print(num1+num2)
+            print(num1+num2) #adds the two numbers
 
         case 2:
-            print(num1-num2)
+            print(num1-num2) #subtracts the two numbers
 
         case 3:
-            print(num1*num2)
+            print(num1*num2) #multiplies the two numbers
 
         case 4:
-            print(num1/num2)
+            print(num1/num2) #divides the two numbers
 
         case _:
-            print("try again sucker")
+            print("try again sucker") #if the number entered is invalid it prints this message
 
     
-    print()
+    print() #prints an extra line to make the output easier to read

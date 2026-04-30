69e486b8792759fcff3dc836dcb9072eb023c6ff
Rylee B <2burtonr@qegs.email>
2026-04-30 16:22:25 +0100

    Enhance gameplay mechanics and user feedback
    Refactor the higher or lower game to improve score tracking and user prompts.

--- higherorlower.py
+++ higherorlower.py
@@ -1,20 +1,32 @@
 import random
 
-print("Welcome to higher or lower")
-print("\nI have thought of a number between 1 and 100")
+print("Welcome to higher or lower.")
+print("\nI have thought of a number between 1 and 100.")
+print("You are aiming for the lowest score.")
 
-number = int(random.randint(1,100))
-print({number})
 while True:
+    score = 0
+    number = int(random.randint(1,100))
     
-    guess = int(input("Make your guess: "))
+    while True:
+        
+        guess = int(input("Make your guess: "))
+        
+        if guess == number:
+            print(f"Well done you guessed the number {number}")
+            break
+        
+        if guess > number:
+            print("Lower")
+            score = score + 1
+            
+        
+        if guess < number:
+            print("Higher")
+            score = score + 1
+            
+        print(f"Your score is: {score}.")
     
-    if guess == number:
-        print("Well done you guessed the number {number}")
-        break
-    
-    if guess >> number:
-        print("Lower")
-    
-    if guess << number:
-        print("Higher")
+    print(f"\nYour final score is: {score}.")
+
+    print("\n\n\nNEW ROUND:\n")
